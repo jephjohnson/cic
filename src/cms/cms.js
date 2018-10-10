38f0de35d931ba4f06d3c6f7518d126a65386813
Jeph Johnson <hello@jephjohnson.com>
2018-10-10 13:11:31 -0700

    Additional Pages

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -4,9 +4,11 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
+import HowPagePreview from './preview-templates/HowPagePreview'
 
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
+CMS.registerPreviewTemplate('how', HowPagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
