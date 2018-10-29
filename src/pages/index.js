240a88a588b3593b23f4f653b85e71fa6ae4f9eb
Jeph Johnson <hello@jephjohnson.com>
2018-10-29 16:14:43 -0400

    Features component

--- src/pages/index.js
+++ src/pages/index.js
@@ -41,7 +41,7 @@ export default class IndexPage extends React.Component {
 													<div className="column is-5-desktop is-offset-6 features--text has-text-centered">
 														<h5>{post.frontmatter.main.image1.title}</h5>
 														<h6>{post.frontmatter.main.image1.subtitle}</h6>
-														<p>{post.frontmatter.main.image1.description}</p>
+														<ReactMarkdown source={ post.frontmatter.main.image1.description } />
 													</div>
 												</div>
 										</div>
@@ -49,10 +49,10 @@ export default class IndexPage extends React.Component {
 								<div className="columns is-centered features">
 										<div className="column is-10-mobile is-10-tablet is-8-widescreen features--outer is-paddingless">
 												<div className="columns features--inner pos-right" style={{ backgroundImage: `url(${post.frontmatter.main.image2.image})` }}>
-													<div className="column is-5-desktop features--text has-text-centered">
+													<div className="column is-5-tablet is-5-desktop features--text has-text-centered">
 														<h5>{post.frontmatter.main.image2.title}</h5>
 														<h6>{post.frontmatter.main.image2.subtitle}</h6>
-														<p>{post.frontmatter.main.image2.description}</p>
+														<ReactMarkdown source={ post.frontmatter.main.image2.description } />
 													</div>
 												</div>
 										</div>
@@ -60,10 +60,10 @@ export default class IndexPage extends React.Component {
 								<div className="columns is-centered features">
 										<div className="column is-10-mobile is-10-tablet is-8-widescreen features--outer is-paddingless">
 												<div className="columns features--inner" style={{ backgroundImage: `url(${post.frontmatter.main.image3.image})` }}>
-													<div className="column is-5-desktop features--text has-text-centered">
+													<div className="column is-5-desktop is-offset-6 features--text has-text-centered">
 														<h5>{post.frontmatter.main.image3.title}</h5>
 														<h6>{post.frontmatter.main.image3.subtitle}</h6>
-														<p>{post.frontmatter.main.image3.description}</p>
+														<ReactMarkdown source={ post.frontmatter.main.image3.description } />
 													</div>
 												</div>
 										</div>
