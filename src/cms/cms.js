705816109eb3e99a3427ee0b27a0e32e341a1b05
Jeph Johnson <hello@jephjohnson.com>
2018-11-14 20:57:55 -0800

    How Page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,6 +2,7 @@ import CMS from 'netlify-cms'
 import * as NativeColorWidget from 'netlify-cms-widget-native-color'
 
 import AboutPagePreview from './preview-templates/AboutPagePreview'
+import HowPagePreview from './preview-templates/HowPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
@@ -11,6 +12,7 @@ import HomePagePreview from './preview-templates/HomePagePreview'
 CMS.registerPreviewStyle('/styles.css')
 CMS.registerWidget('native-color', NativeColorWidget.Control)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
+CMS.registerPreviewTemplate('how', HowPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
 CMS.registerPreviewTemplate('how', HomePagePreview)
