352f3e88703de0a4701d0b7d3fea609c86519f85
Jeph Johnson <hello@jephjohnson.com>
2018-11-14 18:54:06 -0800

    removed how page

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -5,7 +5,6 @@ import AboutPagePreview from './preview-templates/AboutPagePreview'
 import BlogPostPreview from './preview-templates/BlogPostPreview'
 import ProductPagePreview from './preview-templates/ProductPagePreview'
 import ContactPagePreview from './preview-templates/ContactPagePreview'
-import HowPagePreview from './preview-templates/HowPagePreview'
 import HomePagePreview from './preview-templates/HomePagePreview'
 
 
@@ -14,6 +13,5 @@ CMS.registerWidget('native-color', NativeColorWidget.Control)
 CMS.registerPreviewTemplate('about', AboutPagePreview)
 CMS.registerPreviewTemplate('contact', ContactPagePreview)
 CMS.registerPreviewTemplate('products', ProductPagePreview)
-CMS.registerPreviewTemplate('how', HowPagePreview)
 CMS.registerPreviewTemplate('how', HomePagePreview)
 CMS.registerPreviewTemplate('blog', BlogPostPreview)
